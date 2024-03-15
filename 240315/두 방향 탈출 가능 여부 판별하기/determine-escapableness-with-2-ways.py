5cac844b46c7f72acd224d6599e7940fb8c9b721
Jinhoon-Kim <begreat@kakao.com>
2024-03-15 18:43:41 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240315/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -6,14 +6,16 @@ visit = [[False] * n for _ in range(n)]
 can = 0
 def dfs(curr):
     y, x = curr
-    if y == n-1 and x == n1:
+    if y == n-1 and x == n-1:
         can = 1
         return None
         
     for dyx in d:
         dy, dx = dyx
         ny, nx = y + dy, x + dx
-        if 0 <= ny < n and 0 <= nx < n and grid[ny][nx] != 1 and not visit[ny][nx]:
+        if 0 <= ny < n and 0 <= nx < n and grid[ny][nx] != 0 and not visit[ny][nx]:
             visit[ny][nx] = True
             dfs((ny, nx))
+visit[0][0] = True
+dfs((0, 0))
 print(can)
